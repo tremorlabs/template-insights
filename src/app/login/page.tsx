281e73ba216c81d79645090c3ede9af8689f38ae
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-07-16 21:40:34 +0200

    styling: login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,32 +3,47 @@ import { Divider } from "@/components/Divider";
 import { Input } from "@/components/Input";
 import { Label } from "@/components/Label";
 import { Logo } from "@/components/ui/Logo";
+import { RiGithubFill, RiGoogleFill } from '@remixicon/react';
 
 export default function Login() {
   return (
     <>
       <div className="flex min-h-dvh w-dvw items-center justify-center">
-        <div className="flex w-full max-w-xs flex-col items-start">
+        <div className="flex w-full max-w-sm flex-col items-start">
           <div className=" bg-white ring-1 ring-black/5 shadow-lg relative flex items-center justify-center rounded-lg p-3">
             <Logo className="size-8 text-blue-500" />
           </div>
           <div className="mt-6 flex flex-col">
-            <h1 className=" font-medium text-lg text-gray-900">Log in to Insights</h1>
-            <p className="text-sm text-gray-700">
-            Don&rsquo;t have an account?{" "}
-            <a className="text-blue-500 hover:text-blue-600" href="#">
-            Sign up
-            </a>
-          </p>
+            <h1 className="font-semibold text-lg text-gray-900">Log in to Insights</h1>
+            <p className="mt-1 text-sm text-gray-700">
+              Don&rsquo;t have an account?{" "}
+              <a className="text-blue-500 hover:text-blue-600" href="#">
+                Sign up
+              </a>
+            </p>
           </div>
-          <div className="flex mt-12 w-full flex-col gap-y-3">
+          <div className="mt-10 w-full">
+            <div className="sm:flex sm:flex-row sm:items-center gap-2">
+              <Button asChild variant="secondary" className="w-full">
+                <a href="#" className="inline-flex items-center gap-2">
+                  <RiGithubFill className="size-5 shrink-0" aria-hidden="true" />
+                  Login with GitHub
+                </a>
+              </Button>
+              <Button asChild variant="secondary" className="mt-2 w-full sm:mt-0">
+                <a href="#" className="inline-flex items-center gap-2">
+                  <RiGoogleFill className="size-4" aria-hidden="true" />
+                  Login with Google
+                </a>
+              </Button>
+            </div>
+            <Divider className="my-6">or</Divider>
             <form className="flex w-full flex-col gap-y-6">
               <div className="flex flex-col gap-y-4">
                 <div className="flex flex-col space-y-2">
-                  <Label id="email-form-item-label" htmlFor="email-form-item">
+                  <Label id="email-form-item-label" htmlFor="email-form-item" className="font-medium">
                     Email
                   </Label>
-
                   <Input
                     autoComplete="email"
                     name="email"
@@ -36,12 +51,14 @@ export default function Login() {
                     aria-describedby="email-form-item-description"
                     aria-invalid="false"
                     aria-labelledby="email-form-item-label"
+                    placeholder="emily.ross@acme.ch"
                   />
                 </div>
                 <div className="flex flex-col space-y-2">
                   <Label
                     id="password-form-item-label"
                     htmlFor="password-form-item"
+                    className="font-medium"
                   >
                     Password
                   </Label>
@@ -53,6 +70,7 @@ export default function Login() {
                     aria-describedby="password-form-item-description"
                     aria-invalid="false"
                     aria-labelledby="password-form-item-label"
+                    placeholder="Password"
                   />
                 </div>
               </div>
