0d78bc0c6c6cafcc5462316de9cef547305a24ad
severinlandolt <sev.landolt@gmail.com>
2024-08-13 16:51:38 +0200

    update preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { GeistSans } from "geist/font/sans"
+import { ArrowRight } from "lucide-react"
 import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
 import React from "react"
@@ -30,6 +31,23 @@ export const metadata: Metadata = {
   },
 }
 
+function Banner() {
+  return (
+    <div className="fixed bottom-10 left-1/2 z-50 -translate-x-1/2 transition">
+      <div className="flex items-center gap-x-1 rounded-full bg-gray-950 p-1 text-sm shadow-xl shadow-black/20 ring-1 ring-white/10">
+        <a
+          className="group flex items-center gap-0.5 whitespace-nowrap rounded-[20px] bg-gradient-to-b from-white to-gray-200 px-4 py-2 font-semibold text-gray-900 ring-1 ring-inset ring-indigo-400/30 transition"
+          href="https://blocks.tremor.so/templates#template-insights"
+          target="_blank"
+        >
+          Get this template
+          <ArrowRight className="ml-1 size-[18px] shrink-0" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -43,6 +61,7 @@ export default function RootLayout({
       >
         <ThemeProvider defaultTheme="system" attribute="class">
           <div>{children}</div>
+          <Banner />
         </ThemeProvider>
       </body>
     </html>
